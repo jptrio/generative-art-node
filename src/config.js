c3cf4ea7c5c632ef4e21cbdf3e18f3e5c19ee77c
Patrick Gorrell <patrick.gorrell@frst.i>
2021-12-23 14:42:31 -0800

    added more images and commented on code.

--- src/config.js
+++ src/config.js
@@ -1,25 +1,99 @@
-const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+//Super
+// const superMonkz = [
+//     { name: 'background', number: 13 },
+//     { name: 'weapons', number: 8 },
+//     { name: 'supermonks', number: 15 },
+// ];
+
+// //Asian
+// const asianMonkz = [
+//     // { name: 'background', number: 13 },
+//     // { name: 'weapons', number: 8 },
+//     { name: 'skins/asian', number: 1 },
+//     { name: 'mouths/asian', number: 3 },
+//     // { name: 'eyes/asian', number: 5 },
+//     // { name: 'accessories', number: 3 },
+//     // { name: 'jewelry', number: 3 },
+// ];
+
+// //Black
+// const blackMonkz = [
+//     // { name: 'background', number: 13 },
+//     // { name: 'weapons', number: 8 },
+//     // { name: 'skins/black', number: 1 },
+//     // { name: 'mouths/black', number: 3 },
+//     // { name: 'eyes/bw', number: 5 },
+//     // { name: 'accessories', number: 3 },
+//     // { name: 'jewelry', number: 3 },
+// ];
+
+// //White
+// const whiteMonkz = [
+//     // { name: 'background', number: 13 },
+//     // { name: 'weapons', number: 8 },
+//     // { name: 'skins/white', number: 1 },
+//     // { name: 'mouths/white', number: 3 },
+//     // { name: 'eyes/bw', number: 5 },
+//     // { name: 'accessories', number: 3 },
+//     // { name: 'jewelry', number: 3 },
+// ];
+
+// //TiDye
+// const tiDyeMonkz = [
+//     // { name: 'background', number: 13 },
+//     // { name: 'weapons', number: 8 },
+//     // { name: 'skins/tidye', number: 1 },
+//     // { name: 'mouths/tidye', number: 3 },
+//     // { name: 'eyes/bw', number: 5 },
+//     // { name: 'accessories', number: 3 },
+//     // { name: 'jewelry', number: 3 },
+// ];
+
+// RENDER SKINS FIRST
+const renderedSkins = [
+    { name: '_renderedSkins', number: 1 },
+    { name: '_renderedMouths', number: 5 },
 ];
-  
+
+// FINAL
+const cryptoMonkz = [
+    { name: 'background', number: 24 },
+    { name: 'weapons', number: 8 },
+    { name: 'skins', number: 23 },
+    { name: 'accessories', number: 11 },
+    { name: 'clothing', number: 4 },
+    { name: 'jewelry', number: 4 },
+    { name: 'eyes', number: 15 },
+    // { name: 'mouths', number: 12 },
+];
+
 const format = {
-    width: 230,
-    height: 230
+    width: 920,
+    height: 920
 };
 
 const rarity = [
     { key: "", val: "original" },
     { key: "_r", val: "rare" },
     { key: "_sr", val: "super rare" },
+    { key: "_hr", val: "hyper rare" },
 ];
 
-const defaultEdition = 5;
+const edititions = [
+    // { series:'renderedSkins', config: renderedSkins, number: 10 },
+    { series:'cryptoMonkz', config: cryptoMonkz, number: 10 },
+    // { series:'superMonkz', config: superMonkz, number: 100 },
+    // { series:'asianMonkz', config: asianMonkz, number: 30 },
+    // { series:'blackMonkz', config: blackMonkz, number: 15 },
+    // { series:'whiteMonkz', config: whiteMonkz, number: 15 },
+    // { series:'tiDyeMonkz', config: tiDyeMonkz, number: 30 }
+    // { series:'tiDyeMonkz', config: tiDyeMonkz, number: 30 }
+]
 
-module.exports = { layersOrder, format, rarity, defaultEdition };
+const currentEdition = edititions[0];
+
+const series = currentEdition.series;
+const layersOrder = currentEdition.config
+const defaultEdition = currentEdition.number;
+
+module.exports = { layersOrder, format, rarity, defaultEdition, series };
--- src/main.js
+++ src/main.js
@@ -1,26 +1,50 @@
 const fs = require("fs");
-const { createCanvas, loadImage } = require("canvas");
+const {
+  createCanvas,
+  loadImage
+} = require("canvas");
 const console = require("console");
-const { layersOrder, format, rarity } = require("./config.js");
+/* @dev Parse the config file and get all of the corresponding elements needed to create the images and the metadata.json */
+const {
+  layersOrder,
+  format,
+  rarity,
+  series,
+} = require("./config.js");
 
+/* @dev Draw an empty canvas element and set it to be 2 dimensions */
 const canvas = createCanvas(format.width, format.height);
-const ctx = canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
 
+/* @dev Clear the canvas and redraw it */
+const clearCanvas = async () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var w = canvas.width;
+  canvas.width = format.width;
+  canvas.height = format.height;
+  return canvas.getContext("2d");
+}
+
+/* @dev We do not need a .env but if we did, we would set the working directory */
 if (!process.env.PWD) {
   process.env.PWD = process.cwd();
 }
 
-const buildDir = `${process.env.PWD}/build`;
+/* @dev Set the directory and the series path for the new directory that will be created along with the metadata.json */
+const buildDir = `${process.env.PWD}/${series}`;
+/* @dev Set the name of the output metadata.json file */
 const metDataFile = '_metadata.json';
+/* @dev Set the path for saving files in layers directory */
 const layersDir = `${process.env.PWD}/layers`;
 
+/* @dev Metadata is an array */
 let metadata = [];
 let attributes = [];
 let hash = [];
 let decodedHash = [];
 const Exists = new Map();
 
-
+/* @dev This simply puts how rare an itemm is in the metadata.json */
 const addRarity = _str => {
   let itemRarity;
 
@@ -33,6 +57,7 @@ const addRarity = _str => {
   return itemRarity;
 };
 
+/* @dev Remove the rarity from the name of the layer */
 const cleanName = _str => {
   let name = _str.slice(0, -4);
   rarity.forEach((r) => {
@@ -41,6 +66,7 @@ const cleanName = _str => {
   return name;
 };
 
+/* @dev Get the files for each element or layers used in the image creation process */
 const getElements = path => {
   return fs
     .readdirSync(path)
@@ -48,38 +74,51 @@ const getElements = path => {
     .map((i, index) => {
       return {
         id: index + 1,
+        /* @dev Clean the name an parse the rarity */
         name: cleanName(i),
         fileName: i,
+        /* @dev Append the rarity in the metadata.json file */
         rarity: addRarity(i),
       };
     });
 };
 
+/* @dev Creates the object for constructing a layer on the image */
 const layersSetup = layersOrder => {
   const layers = layersOrder.map((layerObj, index) => ({
     id: index,
     name: layerObj.name,
     location: `${layersDir}/${layerObj.name}/`,
     elements: getElements(`${layersDir}/${layerObj.name}/`),
-    position: { x: 0, y: 0 },
-    size: { width: format.width, height: format.height },
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: format.width,
+      height: format.height
+    },
     number: layerObj.number
   }));
-
   return layers;
 };
 
+/* @dev Creates the directory that is populated with rendered images */
 const buildSetup = () => {
   if (fs.existsSync(buildDir)) {
-    fs.rmdirSync(buildDir, { recursive: true });
+    fs.rmdirSync(buildDir, {
+      recursive: true
+    });
   }
   fs.mkdirSync(buildDir);
 };
 
-const saveLayer = (_canvas, _edition) => {
+/* @dev Stores the layer to the .png and writes it to the file */
+const saveLayer = async (_canvas, _edition) => {
   fs.writeFileSync(`${buildDir}/${_edition}.png`, _canvas.toBuffer("image/png"));
 };
 
+/* @dev */
 const addMetadata = _edition => {
   let dateTime = Date.now();
   let tempMetadata = {
@@ -95,6 +134,7 @@ const addMetadata = _edition => {
   decodedHash = [];
 };
 
+/* @dev */
 const addAttributes = (_element, _layer) => {
   let tempAttr = {
     id: _element.id,
@@ -105,9 +145,12 @@ const addAttributes = (_element, _layer) => {
   attributes.push(tempAttr);
   hash.push(_layer.id);
   hash.push(_element.id);
-  decodedHash.push({ [_layer.id]: _element.id });
+  decodedHash.push({
+    [_layer.id]: _element.id
+  });
 };
 
+/* @dev */
 const drawLayer = async (_layer, _edition) => {
   const rand = Math.random();
   let element =
@@ -115,53 +158,61 @@ const drawLayer = async (_layer, _edition) => {
   if (element) {
     addAttributes(element, _layer);
     const image = await loadImage(`${_layer.location}${element.fileName}`);
-
-    ctx.drawImage(
+    await ctx.drawImage(
       image,
       _layer.position.x,
       _layer.position.y,
       _layer.size.width,
       _layer.size.height
     );
-    saveLayer(canvas, _edition);
+    await saveLayer(canvas, _edition);
   }
 };
 
+/* @dev */
 const createFiles = async edition => {
+  /* @dev */
   const layers = layersSetup(layersOrder);
-
+  /* @dev We set the number of duplicates to prevent an infinite loop. The number of retries shuold never exceed the max available */
   let numDupes = 0;
- for (let i = 1; i <= edition; i++) {
-   await layers.forEach(async (layer) => {
-     await drawLayer(layer, i);
-   });
 
-   let key = hash.toString();
-   if (Exists.has(key)) {
-     console.log(
-       `Duplicate creation for edition ${i}. Same as edition ${Exists.get(
+  /* @dev For each monk in each edition */
+  for (let i = 1; i <= edition; i++) {
+    await layers.forEach(async (layer) => {
+      await drawLayer(layer, i);
+      /* @dev Clear the canvas to write a new image */
+      await clearCanvas();
+    });
+    let key = hash.toString();
+    if (Exists.has(key)) {
+      console.log(
+        `Duplicate creation for edition ${i}. Same as edition ${Exists.get(
          key
        )}`
-     );
-     numDupes++;
-     if (numDupes > edition) break; //prevents infinite loop if no more unique items can be created
-     i--;
-   } else {
-     Exists.set(key, i);
-     addMetadata(i);
-     console.log("Creating edition " + i);
-   }
- }
+      );
+      numDupes++;
+      if (numDupes > edition) break; //prevents infinite loop if no more unique items can be created
+      i--;
+    } else {
+      Exists.set(key, i);
+      addMetadata(i);
+      console.log("Creating edition " + i);
+    }
+  }
 };
 
 const createMetaData = () => {
   fs.stat(`${buildDir}/${metDataFile}`, (err) => {
-    if(err == null || err.code === 'ENOENT') {
+    if (err == null || err.code === 'ENOENT') {
       fs.writeFileSync(`${buildDir}/${metDataFile}`, JSON.stringify(metadata, null, 2));
     } else {
-        console.log('Oh no, error: ', err.code);
+      console.log('Oh no, error: ', err.code);
     }
   });
 };
 
-module.exports = { buildSetup, createFiles, createMetaData };
+module.exports = {
+  buildSetup,
+  createFiles,
+  createMetaData
+};
